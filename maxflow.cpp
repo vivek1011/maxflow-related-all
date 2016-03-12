967f69d8322da01eec0148e1aafcb2b40be460b4
Vivek Bansal <bansalvivek79@gmail.com>
2016-03-12 19:47:48 +0530

    Update maxflow.cpp

--- maxflow.cpp
+++ maxflow.cpp
@@ -1,5 +1,8 @@
 http://www.spoj.com/problems/POTHOLE/ - Ford Fulkerson algorithm implementation basic - O(Maxflow * Edges);
 
 A bipartite graph is graph which can be divided into two disjoint sets such that A intersection B = phi . and no cycles of odd length
-are there in a graph. 
+are there in a graph. and edges are  there only between the two sets .and not within the same set. 
 Check if a graph is bipartite- ? It has no odd cyles. , Coloring is possible with only 2 colours. 
+
+Matching .is a subset of number of edges so taken so that number of edges that are incident or start from any vertex is 
+not greater than one.
